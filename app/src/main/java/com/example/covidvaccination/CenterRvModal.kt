ecc2be8bd03893fce540650fc1d87f46fa4239d0
Urmi Kothari <urmikothari03@gmail.com>
2021-12-06 01:44:06 +0530

    Covid App

---
+++ app/src/main/java/com/example/covidvaccination/CenterRvModal.kt
@@ -0,0 +1,15 @@
+package com.example.covidvaccination
+
+data class CenterRvModal(
+
+
+    val centerName: String,
+    val centerAddress: String,
+    val centerFromTime: String,
+    val centerToTime: String,
+    var fee_type: String,
+    var ageLimit: Int,
+    var vaccineName: String,
+    var availableCapacity: Int
+
+)
